bea4e941969a10acf390a347dc8ff83180d7678f
洪海涛 <=>
2017-03-20 15:17:49 +0800

    2

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -12,13 +12,6 @@ export default class App extends Component {
     return (
       <div id='Hello'>
         <div className='msg'>{this.state.msg}</div>
-        <p className='doc'>
-          <i className='redux'>Redux's</i> <a href='http://redux.js.org/' target='_blank'>doc</a>
-          ( or <a href='http://cn.redux.js.org/' target='_blank'>zh-doc</a> )
-        </p>
-        <p className='doc'>
-          <i className='router'>react-router's</i> <a href='https://reacttraining.com/react-router/web/example/basic' target='_blank'>doc</a>
-        </p>
       </div>
     )
   }
