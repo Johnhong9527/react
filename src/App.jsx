0bd735a45f2773a456594955114afbe1cdd94164
洪海涛 <=>
2017-03-20 15:52:35 +0800

    2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Hello from 'components/Hello'
 import { renderRoutes } from 'react-router-config'
 import { BrowserRouter as Router } from 'react-router-dom'
 import routes from 'routes'
-import logo from './assets/logo.svg'
 import './App.css'
 
 import Counter from 'components/Counter'
@@ -12,9 +11,6 @@ export default class App extends Component {
   render () {
     return (
       <div className='App'>
-        <div className='App-header'>
-          <img src={logo} className='App-logo shake-rotate' alt='logo' />
-        </div>
         <Counter />
         <Hello />
         <Router>
